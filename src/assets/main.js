2f7ab6c97faa0a44b35a43f73b7de6bbe885358c
Melanie <motorresweb@gmail.com>
2017-05-25 12:45:09 -0400

    All Completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,35 @@ $.ajax({
 	url:'https://www.codeschool.com/users/motorresweb.json',
 	dataType: 'jsonp',
 	success: function(response){
-		console.log('response', response);
+	addCourses(response.courses.completed);
 }
-$('#badges').addClass('.course');
-$('.course').css('<h3> + 'title' + </h3>');
+});
+
+function addCourses(courses){
+
+var $badges = $('#badges');
+
+courses.forEach(function(course){
+
+		var $course = $('<div />',{
+		'class': 'course'
+		}).appendTo($badges);
+
+		$('<h3 />',{
+			text: course.title
+		}).appendTo($course);
+
+		$('<img />',{
+			src: course.badge
+		}).appendTo($course);
+
+		$('<a />',{
+			'class': 'btn btn-primary',
+			target: '_blank',
+			href: course.url,
+			text: 'See Course'
+		}).appendTo($course);
+
+		});
+	}
 });
